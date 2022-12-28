4f6d2dbbcf799478554a680bfbc2336c62b4a1ae
beasmm <119599179+beasmm@users.noreply.github.com>
2022-12-28 01:06:19 +0000

    first draft: seg fault, probable cause known

---
+++ projeto2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+
+struct Edge {
+    int u, v, w;
+};
+
+vector<Edge> edges;
+
+vector<int> p (m);
+
+
+int find(int x) {
+    if (x != p[x]) 
+        p[x] = find(p[x]);
+    return p[x];
+}
+
+
+int main() {
+    int total = 0;
+    cin >> n >> m;
+
+    // Loop to read the edges
+    for (int i = 0; i < m; i++) {
+        Edge newEdge;
+        cin >> newEdge.u >> newEdge.v >> newEdge.w;
+        edges.push_back(newEdge);
+    }
+
+    // Sort the edges in increasing order by weight
+    sort(edges.begin(), edges.end(), [](const auto& e1, const auto& e2) {
+        return e1.w < e2.w;
+    });
+
+    for (int i = 0; i < n; i++) {
+        p[i] = i;
+    }
+
+    for (int i = 0; i < m; i++) {
+        int u = edges[i].u;
+        int v = edges[i].v;
+        int w = edges[i].w;
+
+        // Check if adding the edge (u, v) would create a cycle
+        int pu = find(u);
+        int pv = find(v);
+        if (pu != pv) {
+            total += w;
+            p[pu] = pv;
+        }
+    }
+
+    cout << total << endl;
+
+    return 0;
+}
